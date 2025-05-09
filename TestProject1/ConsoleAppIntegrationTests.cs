7eb8e67f9f072faa1d3d5e2c43efd42e95f8f3e9
Rob Smyth <RobSmyth@users.noreply.github.com>
2025-05-09 13:35:35 +1000

    dev: wip

--- TestProject1/ConsoleAppIntegrationTests.cs
+++ TestProject1/ConsoleAppIntegrationTests.cs
@@ -4,11 +4,14 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace OrganisationName.ProductName.ComponentName.TestProject1
 {
     [Parallelizable]
     [TestFixture]
+    [Timeout(30000)]
     internal class ConsoleAppIntegrationTests
     {
         [TestCase(1)]
@@ -25,28 +28,67 @@ namespace OrganisationName.ProductName.ComponentName.TestProject1
         [TestCase(12)]
         [TestCase(13)]
         [TestCase(14)]
+        [Timeout(10000)]
         public void DisplaysHelloMessageWhenRun(int arbitraryValue)
         {
-            var finished = RunApp();
+            var outWriter = new StringWriter();
+            var errorWriter = new StringWriter();
 
+            var finished = RunApp(outWriter, errorWriter);
             Assert.That(finished, Is.True);
+
+            var output = outWriter.ToString();
+            var errorOutput = errorWriter.ToString();
+
+
         }
 
         [Test]
+        [Timeout(10000)]
         public void DisplaysHelloMessageWhenRun02()
         {
-            var finished = RunApp();
+            var outWriter = new StringWriter();
+            var errorWriter = new StringWriter();
 
+
+            var finished = RunApp(outWriter, errorWriter);
             Assert.That(finished, Is.True);
+
+            var output = outWriter.ToString();
+            var errorOutput = errorWriter.ToString();
         }
 
-        private static bool RunApp()
+        private bool RunApp(StringWriter outWriter, StringWriter? errorWriter)
         {
             var process = new Process();
-            process.StartInfo = new ProcessStartInfo("dotnet", "ConsoleApp1.dll");
+            process.StartInfo.FileName = "dotnet";
+            process.StartInfo.Arguments = "ConsoleApp1.dll";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            if (errorWriter != null)
+            {
+                process.ErrorDataReceived += (sender, data) => OnErrorDataReceived(data.Data, errorWriter);
+            }
+
             process.Start();
 
+            process.BeginErrorReadLine();
+            outWriter.Write(process.StandardOutput.ReadToEnd());
+
             return process.WaitForExit(5000);
+        }
+
+        private static void OnErrorDataReceived(string? data, TextWriter errorOut)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            errorOut.WriteLine(data);
         }
     }
 }
